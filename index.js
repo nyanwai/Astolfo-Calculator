5b1cd7d157c21027b8e76753d3bde43fdbd7bde7
nyawai <138332371+nyanwai@users.noreply.github.com>
2024-06-02 18:00:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ function clearDisplay(){
 
 function calculate(){
     try{
-        display.value = eval(display.value);
+        let expression = display.value.trim();
+        if (expression === "10+9" || expression === "9+10") {
+            display.value = "21 :3";
+        } else {
+            display.value = eval(expression);
+        }
     }
     catch{
-        display.value = "Error"
+        display.value = "Error";
     }
 }
